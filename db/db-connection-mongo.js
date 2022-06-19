ef01b78cb2e0ffe2bb2c467b622802791b760614
Estefania <estefania.jimenez@est.iudigital.edu.co>
2022-06-19 17:23:18 -0500

    Se prepara setup para despliegue en heroku

--- db/db-connection-mongo.js
+++ db/db-connection-mongo.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
+require("dotenv").config();
 
 const getConnection = async () => {
     try {
         console.log('Inicializando llamado a bd');
-        await mongoose.connect('mongodb://Estefania:1234567...8@cluster0-shard-00-00.3ialt.mongodb.net:27017,cluster0-shard-00-01.3ialt.mongodb.net:27017,cluster0-shard-00-02.3ialt.mongodb.net:27017/inventario?ssl=true&replicaSet=atlas-6sn6bq-shard-0&authSource=admin&retryWrites=true&w=majority');
+        await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
         console.log('Estoy conectado');
     } catch(error) {
         console.log('Fallo la conexión a la base de datos');
